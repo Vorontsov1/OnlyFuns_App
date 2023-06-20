14af1537bd8165c7455d8d4d4f6fb1cad4c05b4d
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-06-20 12:49:43 +0300

    Post

--- app/user/[id].js
+++ app/user/[id].js
@@ -1,12 +1,14 @@
-import { Text } from "react-native";
-import { useState } from 'react';
+import { Text, View, FlatList } from "react-native";
+import { useState } from "react";
 import { useSearchParams } from "expo-router";
 import users from "../../assets/data/users";
 import UserProfileHeader from "../../src/components/UserProfileHeader";
+import posts from "../../assets/data/posts";
+import Post from "../../src/components/Post";
 
 const ProfilePage = () => {
   const { id } = useSearchParams();
-   const [isSubscribed, setIsSubscribed] = useState(false);
+  const [isSubscribed, setIsSubscribed] = useState(false);
 
   const user = users.find((user) => user.id === id);
 
@@ -14,10 +16,19 @@ const ProfilePage = () => {
     return <Text>User not found</Text>;
   }
 
-  return <UserProfileHeader
-    user={user}
-    isSubscribed={isSubscribed}
-    setIsSubscribed={setIsSubscribed} />;
+  return (
+      <FlatList
+        data={posts}
+        renderItem={({ item }) => <Post post={item} />}
+        ListHeaderComponent={() => (
+          <UserProfileHeader
+            user={user}
+            isSubscribed={isSubscribed}
+            setIsSubscribed={setIsSubscribed}
+          />
+        )}
+      />
+  );
 };
 
 export default ProfilePage;
---
+++ src/components/Post.js
@@ -0,0 +1,73 @@
+import { View, Text, Image } from 'react-native';
+import { Entypo, AntDesign, FontAwesome5 } from "@expo/vector-icons";
+import colors from '../../assets/colors';
+
+
+const Post = ({post}) => {
+  return (
+    <View style={{ marginVertical: 15 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          padding: 10,
+        }}
+      >
+        <Image
+          src={post.User.avatar}
+          style={{
+            width: 50,
+            aspectRatio: 1,
+            borderRadius: 50,
+            marginRight: 10,
+          }}
+        />
+        <View>
+          <Text style={{ fontWeight: "bold", fontSize: 16, marginBottom: 3 }}>
+            {post.User.name}
+          </Text>
+          <Text style={{ color: "gray", fontSize: 14 }}>
+            @{post.User.handle}
+          </Text>
+        </View>
+
+        <View
+          style={{
+            marginLeft: "auto",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ marginRight: 5, color: "gray", fontSize: 14 }}>
+            3 hours ago
+          </Text>
+          <Entypo name="dots-three-horizontal" size={18} color="black" />
+        </View>
+      </View>
+
+      <Text style={{ margin: 10, lineHeight: 18 }}>{post.text}</Text>
+      <Image src={post.image} style={{ width: "100%", aspectRatio: 1 }} />
+
+      <View style={{ margin: 10, flexDirection: "row", alignItems: "center" }}>
+        <AntDesign
+          name="hearto"
+          size={24}
+          color={colors.primary}
+          style={{ marginRight: 15 }}
+        />
+        <FontAwesome5
+          name="dollar-sign"
+          size={22}
+          color={colors.primary}
+          style={{ marginRight: 15 }}
+        />
+      </View>
+
+      <Text style={{ fontWeight: "500", marginHorizontal: 10 }}>
+        {post.likes} Likes
+      </Text>
+    </View>
+  );
+}
+
+export default Post;
