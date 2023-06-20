2351f0d5bc17b4db84521466c63b99c6187884d0
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-06-20 13:04:07 +0300

    profile page

--- app/user/[id].js
+++ app/user/[id].js
@@ -3,8 +3,12 @@ import { useState } from "react";
 import { useSearchParams } from "expo-router";
 import users from "../../assets/data/users";
 import UserProfileHeader from "../../src/components/UserProfileHeader";
+import { FontAwesome5 } from "@expo/vector-icons";
 import posts from "../../assets/data/posts";
 import Post from "../../src/components/Post";
+import colors from "../../assets/colors";
+
+
 
 const ProfilePage = () => {
   const { id } = useSearchParams();
@@ -14,6 +18,38 @@ const ProfilePage = () => {
 
   if (!user) {
     return <Text>User not found</Text>;
+  }
+  if (!isSubscribed) {
+    return (
+      <View>
+        <UserProfileHeader
+          user={user}
+          isSubscribed={isSubscribed}
+          setIsSubscribed={setIsSubscribed}
+        />
+        <View style={{
+          alignItems: 'center',
+          backgroundColor: 'gainsboro',
+          padding: 50,
+          borderRadius: 150,
+        }}>
+          <FontAwesome5
+            name="lock"
+            size={50}
+            color={colors.primary}
+            style={{ marginRight: 15 }}
+          />
+          <Text style={{
+            backgroundColor: colors.primary,
+            padding: 15,
+            height: 50,
+            borderRadius: 25,
+            overflow: 'hidden',
+            margin: 20,
+          }}>Subscribe to see user's posts</Text>
+        </View>
+      </View>
+    );
   }
 
   return (
